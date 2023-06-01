1f650ef0d0b5bafbc73fae792fda964c3bc38920
Gustavo P <gpedr56@gmail.com>
2023-06-01 19:45:51 -0300

    Corrigindo algumas dependencias

--- HeroAPI/configuration/src/main/java/com/hero/MainApplicationConfiguration.java
+++ HeroAPI/configuration/src/main/java/com/hero/MainApplicationConfiguration.java
@@ -3,8 +3,11 @@ package com.hero;
 import com.gusplf.config.PersistenceAdapterConfiguration;
 import com.gusplf.config.WebAdapterConfiguration;
 import com.gusplf.ports.in.FindHeroByIdUseCase;
+import com.gusplf.ports.in.FindHeroByNameUseCase;
 import com.gusplf.ports.out.FindHeroByIdPort;
+import com.gusplf.ports.out.FindHeroByNamePort;
 import com.gusplf.services.FindHeroByIdService;
+import com.gusplf.services.FindHeroByNameService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -16,6 +19,10 @@ public class MainApplicationConfiguration {
     @Bean
     public FindHeroByIdUseCase findHeroByIdUseCase(FindHeroByIdPort findHeroByIdPort) {
         return new FindHeroByIdService(findHeroByIdPort);
+    }
+    @Bean
+    public FindHeroByNameUseCase findHeroByNameUseCase(FindHeroByNamePort findHeroByNamePort) {
+        return new FindHeroByNameService(findHeroByNamePort);
     }
 
 }
--- HeroAPI/web-adapter/src/main/java/com/gusplf/config/WebAdapterConfiguration.java
+++ HeroAPI/web-adapter/src/main/java/com/gusplf/config/WebAdapterConfiguration.java
@@ -1,27 +1,31 @@
 package com.gusplf.config;
 
 import com.gusplf.controllers.FindHeroByIdController;
+import com.gusplf.controllers.FindHeroByNameController;
 import com.gusplf.ports.in.FindHeroByIdUseCase;
-import com.gusplf.ports.out.FindHeroByIdPort;
-import com.gusplf.services.FindHeroByIdService;
+import com.gusplf.ports.in.FindHeroByNameUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebAdapterConfiguration {
     private final FindHeroByIdUseCase findHeroByIdUseCase;
+    private final FindHeroByNameUseCase findHeroByNameUseCase;
 
-    public WebAdapterConfiguration(FindHeroByIdUseCase findHeroByIdUseCase) {
+    public WebAdapterConfiguration(FindHeroByIdUseCase findHeroByIdUseCase, FindHeroByNameUseCase findHeroByNameUseCase) {
         this.findHeroByIdUseCase = findHeroByIdUseCase;
-    }
-
-    @Bean
-    public FindHeroByIdUseCase findHeroByIdUseCase(FindHeroByIdPort findHeroByIdPort) {
-        return new FindHeroByIdService(findHeroByIdPort);
+        this.findHeroByNameUseCase = findHeroByNameUseCase;
     }
 
     @Bean
     public FindHeroByIdController findHeroByIdController() {
         return new FindHeroByIdController(findHeroByIdUseCase);
     }
+
+    @Bean
+    public FindHeroByNameController findHeroByNameController() {
+        return new FindHeroByNameController(findHeroByNameUseCase);
+    }
+
+
 }
